aca22347b702d98aab75563a0828a4b0197c5947
Morrowind1983 <Morrowind1983@gmail.com>
2017-12-22 20:44:30 +0800

    Update WEEK4

--- COURSE 1 - Programming for Everybody/WEEK 2 - Installing and Using Python/first_program.py
+++ COURSE 1 - Programming for Everybody/WEEK 2 - Installing and Using Python/first_program.py
@@ -1,2 +1,6 @@
+# Please upload an image showing your Python code running in the command line or
+# terminal. Please include both the python command as well as the output from
+# the program.
+
 print("I'm Li Jia.")
 print("This is my first Python program.")
