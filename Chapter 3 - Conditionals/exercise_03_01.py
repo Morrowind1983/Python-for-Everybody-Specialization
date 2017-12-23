2d5a269c3f772a2d3074b201da1caf4a612d7ac4
morrowind1983 <morrowind1983@gmail.com>
2017-12-23 15:59:07 +0800

    Add Chapter 4

--- Chapter 3 - Conditionals/exercise_03_01.py
+++ Chapter 3 - Conditionals/exercise_03_01.py
@@ -1,5 +1,9 @@
 # Exercise 1: Rewrite your pay computation to give the employee 1.5 times the
 # hourly rate for hours worked above 40 hours.
+# 
+# Enter Hours: 45
+# Enter Rate: 10
+# Pay: 475.0
 
 raw_hours = input("Enter Hours:")
 hours = float(raw_hours)
